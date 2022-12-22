c54697b5dccf51c36d869d5bd0c1c865eb15b8f5
devgioele <gioele@devgioele.xyz>
2022-12-22 18:51:10 +0100

    Adjust eww to laptop

--- .config/eww/scripts/keyboard_layout.sh
+++ .config/eww/scripts/keyboard_layout.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-keyboard="4617:8705:Dygma_Raise_Keyboard"
+keyboard="1:1:AT_Translated_Set_2_keyboard"
 
 # Print current value now
-swaymsg -t get_inputs | jq -r --unbuffered ".[] | select(.identifier == \"$keyboard\").xkb_active_layout_name"
+swaymsg -t get_inputs | jq -r ".[] | select(.identifier == \"$keyboard\").xkb_active_layout_name"
 # Listen for changes
 swaymsg -m -t subscribe '["input"]' | jq -r --unbuffered "select(.input.identifier == \"$keyboard\").input.xkb_active_layout_name" | sed --unbuffered 's/ //'
