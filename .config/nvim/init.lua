4659f8a773ece4a925d3dd791dff41155d988c5d
devgioele <gioele@devgioele.xyz>
2023-09-26 23:19:55 +0200

    reset neovim setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,22 @@
-require('user/plugins')
 require('user/options')
 require('user/keymaps')
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins", {
+  install = {
+    -- try to load one of these colorschemes when starting an installation during startup
+    colorscheme = { "tokyonight", "habamax" },
+  }
+})
