2fce59bfb41ef60b13b0c5f34b72cda9bf484ebd
devgioele <gioele@devgioele.xyz>
2023-11-16 14:32:05 +0100

    reduce max file size for treesitter to 50kB

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -60,7 +60,7 @@ vim.defer_fn(function()
     highlight = {
       enable = true,
       disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
+        local max_filesize = 50 * 1024 -- 50 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
           return true
